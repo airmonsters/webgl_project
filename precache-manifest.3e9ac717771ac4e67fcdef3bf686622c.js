02ea0f476be5696ddbc5024ac8a5d8047c8e315b
airmonsters <lai2009123@gmail.com>
2020-04-28 00:48:46 -0700

    updated with react

---
+++ precache-manifest.3e9ac717771ac4e67fcdef3bf686622c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd985cf552ca546dd3dcd095de2b6c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55cd331e7e5bd8dce53a",
+    "url": "/static/css/main.2e9f44bf.chunk.css"
+  },
+  {
+    "revision": "79bdd0b90d33fa7d07b0",
+    "url": "/static/js/2.acb255a8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.acb255a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cd331e7e5bd8dce53a",
+    "url": "/static/js/main.54e21a0a.chunk.js"
+  },
+  {
+    "revision": "d04fac08b2c8611c842e",
+    "url": "/static/js/runtime-main.7a4e2425.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
---
+++ precache-manifest.6d9943645159902feaf9b77823704474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07c338ed312eb0ac28b2a5ca02de712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "986393726cc8bb6d570e",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "986393726cc8bb6d570e",
+    "url": "/static/js/2.3332f331.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3332f331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32a7fa4c2dfdee661da",
+    "url": "/static/js/main.73b27507.chunk.js"
+  },
+  {
+    "revision": "b430fa1f598cfbd4159d",
+    "url": "/static/js/runtime-main.3446f640.js"
+  }
+]);
